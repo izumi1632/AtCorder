bf2863684f80e4c4e27f86bc0ca8d45ed3e3b65a
izumi1632 <rendell0119@gmail.com>
2019-07-04 23:24:17 +0900

    ABC128C bit全探索

--- AtCorder/AtCorder/ABCProblems/ABC128.cs
+++ AtCorder/AtCorder/ABCProblems/ABC128.cs
@@ -24,13 +24,51 @@ namespace AtCorder.ABC
         /// </summary>
         public override void C()
         {
-        
+            var NM = ReadSplitInt();
+            var N = NM[0];
+            var M = NM[1];
+            var k = ReadSplitIntLines(M);
+            var p = ReadSplitInt();
 
+            long res = 0;
 
+            // 0001 0010 0011
+            for (int bit = 0; bit < (1 << N); bit++)
+            {
+                bool ok = true;
+                for (int i = 0; i < M; i++)
+                {
+                    int con = 0;
+                    foreach (var item in k[i].Skip(1))
+                    {
+                        // 0001をどれだけずらすか。
+                        // 入力が1のときはずらさないので-1しとく　　
+                        if ((bit & (1 << (item - 1))) != 0)
+                        {
+                            // 0以外ならスイッチオン
+                            // オンの数を数える
+                            con++;
+                        }
+                    }
 
+                    // 偶奇一致判定
+                    if (con % 2 != p[i])
+                    {
+                        ok = false;
+                    }
+                }
 
+                // m行検査してなお偶奇の判定を満たしてるなら
+                // 組み合わせの個数をカウントアップ
+                if (ok)
+                {
+                    res++;
+                }
+            }
 
+            Console.WriteLine(res);
         }
+
 
         public override void D()
         {
