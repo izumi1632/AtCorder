a9386f783a37e0797d1a75936e1660dc5c135c1f
izumi1632 <rendell0119@gmail.com>
2019-07-06 14:11:13 +0900

    ARV061A

--- AtCorder/AtCorder/ABCProblems/ARC061.cs
+++ AtCorder/AtCorder/ABCProblems/ARC061.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using AtCorder.ABCProblems.Base;
 
 namespace AtCorder.ABCProblems
@@ -10,8 +11,35 @@ namespace AtCorder.ABCProblems
         }
 
         public override void A()
-        {
-            throw new NotImplementedException();
+        {  // ARC061A
+
+
+            var S = Console.ReadLine();
+            var k = S.Count() - 1;
+            long total = 0;
+
+            for (int i = 0; i < (1 << k); i++)
+            {
+                var s = long.Parse(S[0].ToString());
+
+                for (int j = 0; j < k; j++)
+                {
+                    // 前からjからjけた目の右側にプラスにが存在するかどうかを論理積とって判断する。
+                    if ((i & (1 << j)) != 0)
+                    {
+                        total += s;
+                        s = 0;
+                    }
+
+                    // + が存在しない場合は数字を積み上げるので１０倍し、合計に足さないで保持して次のけたの数字と連結
+                    // ↑で＋されている場合は０なので単純に次けたの数値を取得しているだけ。
+                    s = ((s * 10) + (long.Parse(S[j + 1].ToString())));
+
+                }
+
+                total += s;
+            }
+            Console.WriteLine(total);
         }
 
         public override void B()
--- AtCorder/AtCorder/Program.cs
+++ AtCorder/AtCorder/Program.cs
@@ -11,35 +11,7 @@ namespace AtCorder
         static void Main(string[] args)
         {
 
-            // ARC061A
-
-
-            var S = Console.ReadLine();
-            var k = S.Count() - 1;
-            int a = 0;
-
-            for (int i = 0; i < (1 << k); i++)
-            {
-                int s = int.Parse(S[0].ToString());
-
-                for (int j = 0; j < k; j++)
-                {
-                    // 前からjからjけた目の右側にプラスにが存在するかどうかを論理積とって判断する。
-                    if ((i & (1 << j)) != 0)
-                    {
-                        a += s;
-                        s = 0;
-                    }
-
-                    // + が存在しない場合は数字を積み上げるので１０倍し、合計に足さないで保持して次のけたの数字と連結
-                    // ↑で＋されている場合は０なので単純に次けたの数値を取得しているだけ。
-                    s += s * 10 + int.Parse(S[j + 1].ToString());
-
-                }
-
-                a += s;
-            }
-            Console.WriteLine(a);
+          
 
             Console.ReadKey();
         }
